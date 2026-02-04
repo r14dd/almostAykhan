5b1aa290655aaefe3a3aecb5dc07216fe6a4cfb4
r14dd <riadmukh@buffalo.edu>
2026-02-05 01:56:49 +0400

    feat: added line extraction and cleaning logic, tested accordingly

--- scraper/parser.py
+++ scraper/parser.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 def remove_tags(_soup: BeautifulSoup) -> None:
     """
@@ -10,3 +10,64 @@ def remove_tags(_soup: BeautifulSoup) -> None:
     
     for tag in _soup.find_all(no_content_tags):
         tag.decompose()
+
+def get_text_len(_tag) -> int:
+    """
+    Input: tag
+    Output: Length of the clean text inside a given tag
+    """
+    return len(_tag.get_text(separator=" ", strip=True))
+
+def find_candidates(_soup: BeautifulSoup):
+    """
+    Input: soup
+    Output: List of tags that mightve contained needed content
+    """
+    return _soup.find_all(["main", "article", "section", "div"])
+
+def pick_best_container(_soup: BeautifulSoup):
+    """
+    Input: soup
+    Output: Container with the most text
+    """
+
+    candidates = find_candidates(_soup)
+
+    if candidates:
+        return max(candidates, key=get_text_len)
+    
+    if _soup.body:
+        return _soup.body
+
+    return _soup
+
+def worth_keeping(_text: str):
+    """
+    Input: text, usually one line
+    Output: True if the line of text is worth keeping, False if not
+    """
+    return len(_text.strip().split()) >= 3
+
+def extract_clean_lines(_container: Tag):
+    """
+    Input: container tag
+    Output: list of readable text lines
+    """
+    relevant_tags: list[Tag] = _container.find_all(["h1", "h2", "h3", "p", "li"])
+
+    lines: list[str] = []
+
+    for i in relevant_tags:
+        text: str = i.get_text(strip=True)
+
+        if not text:
+            continue
+
+        if worth_keeping(text):
+            lines.append(text)
+
+    return lines
+
+
+
+
--- scraper/test/test_parser.py
+++ scraper/test/test_parser.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from scraper.parser import remove_tags
+from scraper.parser import *
 
 def test_remove_script():
     html = """
@@ -28,3 +28,66 @@ def test_remove_script():
     assert "Meni hech vaxt silme ha." in text
 
     print("remove tags test passed")
+
+
+
+def test_pick_best_container():
+    html = """
+    <html>
+        <body>
+            <nav>Menu</nav>
+            <div>
+                bla bla blaa.
+            </div>
+
+            <section>
+                <h1>Main Title</h1>
+                <p>
+                    Maraqlı fürsətləri araşdırın, karyeranızı 
+                    inkişaf etdirin və təsir göstərin. Bu gün 
+                    ABB Karyera Platformasında yenilikçi komandamıza qoşulun!
+                </p>
+            </section>
+            <footer>Footer info</footer>
+        </body>
+    </html>
+    """
+
+    soup = BeautifulSoup(html, "html.parser")
+    container = pick_best_container(soup)
+
+    text = container.get_text(separator=" ", strip=True)
+
+    assert "Main Title" in text
+    assert "inkişaf etdirin və təsir göstərin" in text
+    print("container test passed")
+
+
+def test_extract_clean_lines():
+    html = """
+    <html>
+        <body>
+            <h1>Welcome to ABB</h1>
+            <p>Hi</p>
+            <p>This is a proper paragraph with enough wordsdss</p>
+            <ul>
+                <li>Short</li>
+                <li>This list item has meaningful content.</li>
+            </ul>
+        </body>
+    </html>
+    """
+
+    soup = BeautifulSoup(html, "html.parser")
+    lines = extract_clean_lines(soup)
+
+    assert "Welcome to ABB" in lines
+    assert "This is a proper paragraph with enough wordsdss" in lines
+    assert "This list item has meaningful content." in lines
+
+    # filtered out
+    assert "Hi" not in lines
+    assert "Short" not in lines
+        
+    print("clean lines test passed")
+    
